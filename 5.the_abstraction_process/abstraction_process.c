c080fa8241d42c6017fa59021edb79b44efaf731
Victor_Victus <caipoccoa@outlook.com>
2026-03-12 18:27:40 -0500

    process

---
+++ 5.the_abstraction_process/abstraction_process.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+Process: it is a running program
+Often we deployed hundreds of process at once. The OS creates the illusion by
+virtualizing the CPU (running  one process, then stopping it and running another)
+We achive that using a basic technique known as time sharing. To implement 
+virtualization well we need low-level machinery (mechanisims) and high level
+intelligence (policies).
+
+*/
+
+int main(int argc, char *argv[]){
+
+
+
+    return 0;
+}
